7f8530d16a40dcf9a535ab9789629e463a92adf9
renan-cavalcante <renancggama@gmail.com>
2023-08-01 22:35:48 -0300

    refatorando findById de SellerDaoJDBC

--- src/dao/impl/SellerDaoJDBC.java
+++ src/dao/impl/SellerDaoJDBC.java
@@ -55,16 +55,8 @@ public class SellerDaoJDBC implements GenericDao<Seller> {
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if(rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller seller = new Seller();
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				seller.setBirthDate(LocalDate.parse(rs.getDate("BirthDate").toString()));
-				seller.setDepartment(dep);
+				Department dep = instantiateDepartment(rs);
+				Seller seller = instantiateDepartment(rs, dep);
 				return seller;
 			}
 			return null;
@@ -78,6 +70,22 @@ public class SellerDaoJDBC implements GenericDao<Seller> {
 			DB.closeResultSet(rs);
 		}
 		
+	}
+
+	private Seller instantiateDepartment(ResultSet rs, Department dep) throws SQLException {
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setBirthDate(LocalDate.parse(rs.getDate("BirthDate").toString()));
+		seller.setDepartment(dep);
+		return seller;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		return  new Department((rs.getInt("DepartmentId")),(rs.getString("DepName")));
+				
 	}
 
 	@Override
